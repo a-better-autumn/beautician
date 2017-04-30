7e0692debabb98e3a617c5fac4829b268e825a6e
Autumn <abetterautumn@gmail.com>
2017-04-30 15:56:41 +0800

    add seed and add dev cmd

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Hello World!'
+puts '這個種子檔會自動建立一個帳號, 並且随机創建 10 個jobs，10个隐藏的jobs'
+
+create_account = User.create([email: 'user1@gmail.com', password: '654321', password_confirmation: '654321', is_admin: 'true'])
+puts 'Admin account is created successfully!'
+
+job_info = [
+  '初级美容师',
+  '高级美容师',
+  '见习美容师',
+  '兼职美容师',
+  '美容助理'
+]
+create_jobs = for i in 1..10 do
+                Job.create!([title: job_info[rand(job_info.length)], description: "这是一个公开的工作", wage_upper_bound: rand(40..79) * 1000, wage_lower_bound: rand(20..39) * 1000, is_hidden: 'false'])
+              end
+for i in 1..10 do
+  Job.create!([title: job_info[rand(job_info.length)], description: "这是一个隐藏的工作", wage_upper_bound: rand(40..79) * 1000, wage_lower_bound: rand(20..39) * 1000, is_hidden: 'true'])
+end
+
+puts '10 Public jobs created.'
+puts '10 Hidden jobs created.'
